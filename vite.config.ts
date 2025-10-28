a6ccfeb86037707c1aabc7d67123e13d31fad953
Kenneth Law <ken.y.law@gmail.com>
2025-10-28 14:19:47 +0800

    Fix vite build: Remove non-existent vite-plugin-source-identifier dependency

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,7 @@
 import path from "path"
 import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
-import sourceIdentifierPlugin from 'vite-plugin-source-identifier'
 
-const isProd = process.env.BUILD_MODE === 'prod'
 export default defineConfig({
   plugins: [
     react({
@@ -11,11 +9,6 @@ export default defineConfig({
       babel: {
         plugins: []
       }
-    }), 
-    sourceIdentifierPlugin({
-      enabled: !isProd,
-      attributePrefix: 'data-matrix',
-      includeProps: true,
     })
   ],
   resolve: {
